2aafb3ee4b45e172d916238865f562fb22e57db1
Boris <borisgk98@ya.ru>
2019-10-07 18:27:01 +0300

    Добавьте файлы проекта.

---
+++ ParallelProgItis/ParallelProgItis.cpp
@@ -0,0 +1,12 @@
+﻿// ParallelProgItis.cpp: определяет точку входа для приложения.
+//
+
+#include "ParallelProgItis.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
