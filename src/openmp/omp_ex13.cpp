a0f4e06feb6415d0e6b94b31f09aa9969103d1a1
borisgk98 <you@example.com>
2019-10-16 06:30:47 +0400

    solve ex13

--- src/openmp/omp_ex13.cpp
+++ src/openmp/omp_ex13.cpp
@@ -35,17 +35,118 @@ void s2() {
 }
 
 void s3() {
-#pragma omp parallel for schedule(static, 1)
-    for (int i = 0; i < 8; i++) {
-#pragma omp ordered
-        {};
-        out();
+    int myid, i;
+#pragma omp parallel private(myid)
+    {
+        myid = omp_get_thread_num();
+#pragma omp for private(i) ordered
+        for(i=0; i<8; i++)
+            #pragma omp ordered
+                printf("T%d: %d\n", 8 - myid, 8 - i);
+}
+}
+
+void interesting_func(int i) {
+    if (i == -1) {
+        return;
     }
+    #pragma omp parallel
+    {
+        if (i == omp_get_thread_num()) {
+            out();
+        }
+    }
+    interesting_func(i - 1);
+}
+
+void s4() {
+    interesting_func(7);
+}
+
+void s5() {
+    int current = 7;
+#pragma omp parallel sections
+    {
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+    }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+#pragma omp section
+        {
+            while (omp_get_thread_num() != current);
+#pragma omp critical
+            {
+                out();
+                current--;
+            };
+        }
+    };
 }
 
 void omp_ex13() {
     omp_set_num_threads(8);
 //    s1();
 //    s2();
-    s3();
+//    s3();
+//    s4();
+    s5();
 }
